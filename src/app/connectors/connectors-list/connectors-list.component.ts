243838372fbf1936b7b5ddb6aa1d2b24b031edcd
Edson Vladimir Tarqui <edsonvladimir@edsonvladimir.BAGOLIN>
2018-07-24 13:56:02 -0400

    Form details
    	modified:   ../src/app/app.component.css
    	modified:   ../src/app/app.component.html
    	modified:   ../src/app/app.module.ts
    	modified:   ../src/app/connectors/connectors-details/connectors-details.component.css
    	modified:   ../src/app/connectors/connectors-details/connectors-details.component.html
    	modified:   ../src/app/connectors/connectors-list/connectors-list.component.css
    	modified:   ../src/app/connectors/connectors-list/connectors-list.component.html
    	modified:   ../src/app/connectors/connectors-list/connectors-list.component.ts

--- src/app/connectors/connectors-list/connectors-list.component.ts
+++ src/app/connectors/connectors-list/connectors-list.component.ts
@@ -8,7 +8,12 @@ import { Connector } from '../connectors.model';
 })
 export class ConnectorsListComponent implements OnInit {
   connectors: Connector[]=[
+    new Connector('Google Connector','This is a sample connector','GET','www.facebook.com','https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png'),
     new Connector('Facebook Connector','This is a sample connector','GET','www.facebook.com','https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/240px-Facebook_logo_%28square%29.png'),
+    new Connector('Twitter Connector','This is a sample connector','GET','www.facebook.com','https://upload.wikimedia.org/wikipedia/fr/c/c8/Twitter_Bird.svg'),
+    new Connector('Twitch Connector','This is a sample connector','GET','www.facebook.com','https://cdn.iconscout.com/public/images/icon/free/png-512/twitch-logo-376c6b40ea97c37d-512x512.png'),
+    new Connector('Twitch Connector','This is a sample connector','GET','www.facebook.com','https://cdn.iconscout.com/public/images/icon/free/png-512/twitch-logo-376c6b40ea97c37d-512x512.png')
+
   ]
   constructor() { }
 
