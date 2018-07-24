c13fc8cf07c28203efb4f1125fa6ca3cb40a6595
EdsonT <ednoosde.wob@gmail.com>
2018-07-24 01:17:23 -0400

    layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { MatCheckboxModule} from '@angular/material/checkbox'
+
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '../../node_modules/@angular/forms';
+import { ConnectorsComponent } from './connectors/connectors.component';
+import { ConnectorsListComponent } from './connectors/connectors-list/connectors-list.component';
+import { ConnectorsDetailsComponent } from './connectors/connectors-details/connectors-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectorsComponent,
+    ConnectorsListComponent,
+    ConnectorsDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/connectors/connectors-details/connectors-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connectors-details',
+  templateUrl: './connectors-details.component.html',
+  styleUrls: ['./connectors-details.component.css']
+})
+export class ConnectorsDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
